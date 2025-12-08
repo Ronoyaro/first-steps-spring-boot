a1afcac3d1112598c049ec0e5dfdaf076017d1ab
Ronoyaro <ronaldferreiraig@hotmail.com>
2025-12-08 13:15:24 -0300

    video14: aprendizado sobre PostMapping e RequestBody

--- anime-service/src/main/java/study/ronoyaro/controller/HelloController.java
+++ anime-service/src/main/java/study/ronoyaro/controller/HelloController.java
@@ -1,12 +1,25 @@
 package study.ronoyaro.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.ThreadLocalRandom;
+
 
 @RestController
+@RequestMapping("v1/greetings")
+@Slf4j
 public class HelloController {
     @GetMapping("hi")
     public String hi() {
         return "OMAE WA MO SHINDEIRU";
     }
+
+    @PostMapping
+    public Long save(@RequestBody String name) {
+        log.info("saved '{}'", name);
+        return ThreadLocalRandom.current().nextLong(1, 1000);
+
+    }
+
 }
